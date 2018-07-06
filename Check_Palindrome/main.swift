52bcd9a61a38ec3aaa1f9173a8689c4dbabb2ff1
Andy <ajlewis1@me.com>
2018-07-06 08:47:08 -0600

    Initial Commit

---
+++ Check_Palindrome/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Check_Palindrome
+//
+//  Created by Andrew Lewis on 7/6/18.
+//  Copyright © 2018 Andrew Lewis. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
